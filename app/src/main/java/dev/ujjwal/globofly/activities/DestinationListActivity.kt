dcbbc824324824dfba4358a272017389b912b4b9
ujjwal <ujjwalmaity98@gmail.com>
2020-03-27 21:12:47 +0530

    Request and Receive HTTP Response

--- app/src/main/java/dev/ujjwal/globofly/activities/DestinationListActivity.kt
+++ app/src/main/java/dev/ujjwal/globofly/activities/DestinationListActivity.kt
@@ -6,7 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import dev.ujjwal.globofly.R
 import dev.ujjwal.globofly.helpers.DestinationAdapter
 import dev.ujjwal.globofly.helpers.SampleData
+import dev.ujjwal.globofly.models.Destination
+import dev.ujjwal.globofly.services.DestinationService
+import dev.ujjwal.globofly.services.ServiceBuilder
 import kotlinx.android.synthetic.main.activity_destiny_list.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class DestinationListActivity : AppCompatActivity() {
@@ -31,7 +37,21 @@ class DestinationListActivity : AppCompatActivity() {
     }
 
     private fun loadDestinations() {
-        // To be replaced by retrofit code
-        destiny_recycler_view.adapter = DestinationAdapter(SampleData.DESTINATIONS)
+        val destinationService = ServiceBuilder.buildService(DestinationService::class.java)
+
+        val requestCall = destinationService.getDestinationList()
+
+        requestCall.enqueue(object : Callback<List<Destination>> {
+
+            override fun onFailure(call: Call<List<Destination>>, t: Throwable) {
+            }
+
+            override fun onResponse(call: Call<List<Destination>>, response: Response<List<Destination>>) {
+                if (response.isSuccessful) {
+                    val destinationList = response.body()!!
+                    destiny_recycler_view.adapter = DestinationAdapter(destinationList)
+                }
+            }
+        })
     }
 }
