e18c1a97254f73425b59b26d8c3a53b67e66a0a2
ujjwal <ujjwalmaity98@gmail.com>
2020-03-27 22:56:12 +0530

    Using path parameter

--- app/src/main/java/dev/ujjwal/globofly/activities/DestinationDetailActivity.kt
+++ app/src/main/java/dev/ujjwal/globofly/activities/DestinationDetailActivity.kt
@@ -3,11 +3,17 @@ package dev.ujjwal.globofly.activities
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import dev.ujjwal.globofly.R
 import dev.ujjwal.globofly.helpers.SampleData
 import dev.ujjwal.globofly.models.Destination
+import dev.ujjwal.globofly.services.DestinationService
+import dev.ujjwal.globofly.services.ServiceBuilder
 import kotlinx.android.synthetic.main.activity_destiny_detail.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class DestinationDetailActivity : AppCompatActivity() {
 
@@ -33,16 +39,30 @@ class DestinationDetailActivity : AppCompatActivity() {
     }
 
     private fun loadDetails(id: Int) {
-        // To be replaced by retrofit code
-        val destination = SampleData.getDestinationById(id)
+        val destinationService = ServiceBuilder.buildService(DestinationService::class.java)
 
-        destination?.let {
-            et_city.setText(destination.city)
-            et_description.setText(destination.description)
-            et_country.setText(destination.country)
+        val requestCall = destinationService.getDestination(id)
 
-            collapsing_toolbar.title = destination.city
-        }
+        requestCall.enqueue(object : Callback<Destination> {
+            override fun onFailure(call: Call<Destination>, t: Throwable) {
+                Toast.makeText(this@DestinationDetailActivity, "Error occurred $t", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onResponse(call: Call<Destination>, response: Response<Destination>) {
+                if (response.isSuccessful) {
+                    val destination = response.body()
+                    destination?.let {
+                        et_city.setText(destination.city)
+                        et_description.setText(destination.description)
+                        et_country.setText(destination.country)
+
+                        collapsing_toolbar.title = destination.city
+                    }
+                } else {
+                    Toast.makeText(this@DestinationDetailActivity, "Failed to retrieve items", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
     }
 
     private fun initUpdateButton(id: Int) {
--- app/src/main/java/dev/ujjwal/globofly/services/DestinationService.kt
+++ app/src/main/java/dev/ujjwal/globofly/services/DestinationService.kt
@@ -3,10 +3,15 @@ package dev.ujjwal.globofly.services
 import dev.ujjwal.globofly.models.Destination
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface DestinationService {
 
     // http://192.168.43.222:9000/destination
     @GET("destination")
     fun getDestinationList(): Call<List<Destination>>
+
+    // http://192.168.43.222:9000/destination/47
+    @GET("destination/{id}")
+    fun getDestination(@Path("id") id: Int): Call<Destination>
 }
