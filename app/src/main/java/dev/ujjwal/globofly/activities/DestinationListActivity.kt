ac2870a4751503b7fbc8fd28a9af289003afe906
ujjwal <ujjwalmaity98@gmail.com>
2020-03-27 21:34:29 +0530

    Response and error handling

--- app/src/main/java/dev/ujjwal/globofly/activities/DestinationListActivity.kt
+++ app/src/main/java/dev/ujjwal/globofly/activities/DestinationListActivity.kt
@@ -2,6 +2,7 @@ package dev.ujjwal.globofly.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import dev.ujjwal.globofly.R
 import dev.ujjwal.globofly.helpers.DestinationAdapter
@@ -44,12 +45,22 @@ class DestinationListActivity : AppCompatActivity() {
         requestCall.enqueue(object : Callback<List<Destination>> {
 
             override fun onFailure(call: Call<List<Destination>>, t: Throwable) {
+                // Network error OR Establishing connection with server
+                // Error creating http request OR Error processing http response
+                Toast.makeText(this@DestinationListActivity, "Error occurred $t", Toast.LENGTH_SHORT).show()
             }
 
             override fun onResponse(call: Call<List<Destination>>, response: Response<List<Destination>>) {
                 if (response.isSuccessful) {
+                    // Your status code is in the range of 200 to 299
                     val destinationList = response.body()!!
                     destiny_recycler_view.adapter = DestinationAdapter(destinationList)
+                } else if (response.code() == 401) {
+                    Toast.makeText(this@DestinationListActivity, "Your session has expire. Please Login again.", Toast.LENGTH_SHORT).show()
+                } else {
+                    // Application-level failure
+                    // Your status code is in the range of 300's, 400's and 500's
+                    Toast.makeText(this@DestinationListActivity, "Failed to retrieve items", Toast.LENGTH_SHORT).show()
                 }
             }
         })
