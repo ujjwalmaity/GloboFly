6e5e5d8f138b7f4d957b01dc1b5c399293744072
ujjwal <ujjwalmaity98@gmail.com>
2020-03-27 22:10:27 +0530

    Add OkHttpClient

--- app/src/main/java/dev/ujjwal/globofly/services/ServiceBuilder.kt
+++ app/src/main/java/dev/ujjwal/globofly/services/ServiceBuilder.kt
@@ -1,5 +1,6 @@
 package dev.ujjwal.globofly.services
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -7,10 +8,14 @@ object ServiceBuilder {
 
     private const val URL = "http://192.168.43.222:9000/"
 
+    // Create OkHttp Client
+    private val okHttp = OkHttpClient.Builder()
+
     // Create Retrofit Builder
     private val builder = Retrofit.Builder()
         .baseUrl(URL)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttp.build())
 
     // Create Retrofit Instance
     private val retrofit = builder.build()
